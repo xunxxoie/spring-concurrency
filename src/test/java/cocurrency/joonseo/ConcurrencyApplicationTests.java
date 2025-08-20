40a60c2b5cdf9d38891298fd6ef3d0c2b4721df6
xunxxoie <ahh0520@naver.com>
2025-08-20 21:24:21 +0900

    feat: Initial commit

---
+++ src/test/java/cocurrency/joonseo/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package cocurrency.joonseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
